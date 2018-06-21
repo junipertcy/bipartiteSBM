61fffc8e0f7a1f27646c6d78d9d1033dd9a60577
junipertcy <junipertcy@gmail.com>
2018-06-21 11:10:19 -0600

    removed pushd/popd in scripts/compile_engines.sh

--- scripts/compile_engines.sh
+++ scripts/compile_engines.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pushd engines/bipartiteSBM-MCMC/ && cmake . && make && popd
-pushd engines/bipartiteSBM-KL/ && g++ -O3 -Wall -g -pedantic -o biSBM biSBM.cpp && popd
+cd engines/bipartiteSBM-MCMC/ && cmake . && make && cd -
+cd engines/bipartiteSBM-KL/ && g++ -O3 -Wall -g -pedantic -o biSBM biSBM.cpp && cd -
