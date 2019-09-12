15d8b1ed524bbc1863fdbcb0adf9a6d1f4c6930d
Tzu-Chi Yen <tzuchi.yen@colorado.edu>
2019-09-12 00:42:59 -0600

    test

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Add the miniconda bin directory to $PATH
-export PATH=$HOME/miniconda3/bin:$PATH
+#export PATH=$HOME/miniconda3/bin:$PATH
 echo $PATH
 
 # Use the miniconda installer for setup of conda itself
@@ -18,16 +18,16 @@ if [[ ! -f $HOME/miniconda3/bin/activate ]]
             wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
         elif [[ "$TRAVIS_OS_NAME" == "osx" ]]
         then
-            wget http://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
+#            wget http://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
             brew update
             brew upgrade
             brew install boost-python cmake
         fi
     fi
-    chmod +x miniconda.sh && ./miniconda.sh -b -f
-    conda update --yes conda
-    conda create -n testenv36 --yes python=3.6
-    conda create -n testenv37 --yes python=3.7
+#    chmod +x miniconda.sh && ./miniconda.sh -b -f
+#    conda update --yes conda
+#    conda create -n testenv36 --yes python=3.6
+#    conda create -n testenv37 --yes python=3.7
 fi
 cd ..
 popd
