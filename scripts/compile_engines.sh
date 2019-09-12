1af6bf66b0aa3b482627eb2f5368fc14cb863f52
Tzu-Chi Yen <tzuchi.yen@colorado.edu>
2019-09-12 02:30:26 -0600

    test

--- scripts/compile_engines.sh
+++ scripts/compile_engines.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd engines/bipartiteSBM-MCMC/ && cmake . && make && cd -
+cd engines/bipartiteSBM-MCMC/ && cmake -DBOOST_ROOT=/usr/local/opt/boost@1.67 . && make && cd -
 cd engines/bipartiteSBM-KL/ && g++ -O3 -Wall -g -pedantic -o biSBM biSBM.cpp && cd -
